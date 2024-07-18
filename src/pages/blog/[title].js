b21aa63d012124dbd74c0dbd613209613aacacd3
Pushkar Goel <pgpushkar@gmail.com>
2024-07-18 16:15:14 +0530

    Added new blog

--- src/pages/blog/[title].js
+++ src/pages/blog/[title].js
@@ -1,15 +1,17 @@
 import React from "react";
 import Head from "next/head";
 import { useRouter } from 'next/router';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 import Layout from '../../components/Layout';
 import BlogPost from '../../components/BlogPost';
 import { BLOGS } from '../../config/constants';
 
 export default function Blog({ }) {
-    const router = useRouter()
-    const blog = BLOGS.find(blog => blog.title == router.query.title);
-    debugger
+    const router = useRouter();
+    const blogName = router.query.title;
+    const blog = BLOGS.find(blog => blog.title == blogName);
 
     return (
         <Layout>
@@ -18,13 +20,23 @@ export default function Blog({ }) {
                 <title>Truth Wheel: Spin for Random Truths | PickerWheel</title>
             </Head>
 
-            <BlogPost
-                date={blog.date}
-                imageSrc={blog.imageSrc}
-                title={blog.title}
-                subTitle={blog.subTitles}
-                content={blog.content}
-            />
+            {
+                blog ? 
+                <BlogPost
+                    date={blog.date}
+                    imageSrc={blog.imageSrc}
+                    title={blog.title}
+                    subTitle={blog.subTitles}
+                    content={blog.content}
+                /> :
+                <Box 
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    minHeight="80vh">
+                    <Typography variant="h5">Blog not found!</Typography>
+                </Box>
+            }
 
         </Layout>
     );
